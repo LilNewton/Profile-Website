68b3a6bf97fd2f77df79e271ee4c0cdb4348d15f
LilNewton <118666447+LilNewton@users.noreply.github.com>
2026-03-21 18:04:48 +0800

    - Added most of the div structures. - Inserted most of the actual text. - Set up the design tokken system. - Added icon images of tools in a folder.

--- website/resources/views/main.blade.php
+++ website/resources/views/main.blade.php
@@ -5,11 +5,119 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Chris-John-Borigas</title>
+
+    @vite('resources/css/app.css')
 </head>
 
-<body>
-    <h1>"Hello World!"
-    </h1>
+<body class=web-container>
+    <div class="sidebar">
+
+    </div>
+
+    <!--Introduction Section-->
+    <div class="container__section">
+        <h2>Chris John B. Borigas</h2>
+        <h1>Frontend Development</h1>
+        <p>Good Day! I am Chris John Borigas.
+            A frontend developer and Computer Science graduate from
+            Bicol University College of Science with 240 hours of
+            industry experience and 3 notable projects, focusing on
+            building scalable and responsive web solutions.
+        </p>
+        <button>Resume</button>
+
+    </div>
+
+    <!--About Section-->
+    <div class="container__section">
+        <h2>I approach projects with a user-centered mindset,
+            making sure the projects I handle deliver a smooth and
+            straight forward experience for the user.
+        </h2>
+        <h3>About Me</h3>
+        <div class="about-me">
+            <h2>Hello There!</h2>
+            <p>I'm a computer science graduate frontend developer
+                with a user-centered philosophy towards designing
+                the structure of features. <br><br>
+                My approach centers around creating a smooth user
+                experience in consideration with project objectives.
+                Utilizing my knowledge in ideation to iterate and
+                innovate unto the features of projects through its
+                concept, wireframe, and prototype until its release.
+            </p>
+        </div>
+    </div>
+
+    <!--Tools Section-->
+    <div class="container__section">
+        <h3>Tools</h3>
+        <div class="card card_tool">
+            <h2>Languages</h2>
+            <div class="tool-grid">
+
+            </div>
+        </div>
+        <div class="card card_tool">
+            <h2>Frameworks</h2>
+            <div class="tool-grid">
+
+            </div>
+        </div>
+        <div class="card card_tool">
+            <h2>Design</h2>
+            <div class="tool-grid">
+
+            </div>
+        </div>
+        <div class="card card_tool">
+            <h2>Version Control</h2>
+            <div class="tool-grid">
+
+            </div>
+        </div>
+        <div class="card card_tool">
+            <h2>Documentation</h2>
+            <div class="tool-grid">
+
+            </div>
+        </div>
+    </div>
+
+    <!--Projects Section-->
+    <div class="container__section">
+        <h2>Selected Projects</h2>
+        <div class="card card_project">
+
+        </div>
+        <div class="card card_project">
+
+        </div>
+    </div>
+
+    <!--Experience Section-->
+    <div class="container__section">
+        <h2>Experience</h2>
+        <div class="card">
+            <h4>Iraya Energies</h4>
+            <h2>Software Engineer (OJT)</h2>
+            <p>June - July (2024)</p>
+        </div>
+        <div class="card">
+            <h4>Project OLA</h4>
+            <h2>Researcher/Frontend Develoer</h2>
+            <p>Aujust 2024 - May 2025</p>
+        </div>
+    </div>
+
+    <!--Contact Section-->
+    <div class="container__section">
+        <h4>My Contacts</h4>
+        <h3>chrisjohnbborigas@gmail.com</h3>
+        <div class="contact-icons">
+
+        </div>
+    </div>
 </body>
 
 </html>
